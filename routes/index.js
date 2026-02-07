d8c641d40bc7d25d9ba384bec5a8425f5a974549
Angela Murray <angelamurray776@outlook.com>
2026-02-07 09:30:13 -0500

    added welcome message

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const swaggerAutogen = require('swagger-autogen')();
-
-// router.get('/', (req, res) => {
-//   res.send('Welcome to your Local Library Home Page!');
-// });
+const welcomeMessage = `
+    <h1>Welcome to your Local Library Home Page!</h1>
+    <ul>
+      <li>For a list of <strong>books</strong> → <code>/books</code></li>
+      <li>For a list of <strong>users</strong> → <code>/users</code></li>
+      <li>For <strong>API Documentation</strong> → <code>/api-docs</code></li>
+    </ul>`;
+    
+router.get('/', (req, res) => {
+  res.send(welcomeMessage);
+});
 
 router.use('/books', require('./books'));
 router.use('/users', require('./users'));
